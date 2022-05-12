5c63165e8c52afabf0a1e13b1538363b49388321
GaspardMzt <gaspard.mzt@gmail.com>
2022-05-12 21:03:20 +0200

    header change according to URL, now have to put the image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,14 +2,18 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "../styles/header.css";
 import header_wood from "../assets/header_wood.png";
+import { useLocation } from "react-router-dom";
 
 export default function Header() {
+  let location = useLocation();
+
   const headerbackground = {
     backgroundImage: `url(${header_wood})`,
   };
 
   return (
     <div className="header" style={headerbackground}>
+      {location.pathname === "/" ? <div> Accueil </div> : <div>Calcul </div>}
       <Link to="/">
         <h1 className="titleHeader">Cou-a-bou-ar</h1>
       </Link>
