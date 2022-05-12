a7c80a07fbd667ed401c7fae879ab81dfac659c1
Claiirou <clairemachet@hotmail.fr>
2022-05-12 16:46:58 +0200

    adjust background to desktop

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "../styles/footer.css";
+import logo2 from "../assets/logo2.png";
 
 export default function Footer() {
   return (
-    <div>
-      <p>Hello Footer</p>
-    </div>
+    <footer>
+      <Link to="/">
+        <img src={logo2} alt="logo" className="logoImageFooter" />
+      </Link>
+      <p className="footerText">COU-A-BOU-AR © 2022 Tous droits réservés.</p>
+    </footer>
   );
 }
