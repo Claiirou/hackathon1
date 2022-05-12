d7e4f5d57362359813d56ce165245d08fef655cd
PIERRE DARTEVELLE <pdarteve5@gmail.com>
2022-05-12 14:08:57 +0200

    fixed '/' route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Accueil from "./pages/Accueil";
 
 function App() {
-  return <div></div>;
+  return (
+    <div className="routesContainer">
+      <Routes>
+        <Route path="/" element={<Accueil />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
