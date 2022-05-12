5764c0d57c8bc259d0b02aad0bacb068236aec87
PIERRE DARTEVELLE <pdarteve5@gmail.com>
2022-05-12 17:15:49 +0200

    fixed the data

--- src/components/AppelData.jsx
+++ src/components/AppelData.jsx
@@ -1,15 +1,17 @@
-import jsonData from '../assets/data.json'
+import alcool from '../assets/alcools.json'
+import apero from '../assets/apero.json'
+import bieres from '../assets/bieres.json'
+import softs from '../assets/softs.json'
 
 
 const AppelData = () => {
 
-    const alcoholList =jsonData.slice(0, 20);
-    const chipsNCo = jsonData.filter(c => c.nom_francais.includes('chips') || c.nom_francais.includes('Chips'))
-    const peanuts = jsonData.filter(c => c.nom_francais.includes('Cacahuète') && c.nom_francais.includes('grillée'));
-    const nutmix = jsonData.filter(c => c.nom_francais.includes('Mélange apéritif'));
-    const saucisson = jsonData.filter(c => c.nom_francais.includes('Saucisson sec'));
-    const olives = jsonData.filter(c => c.nom_francais.includes('Olive'));
-    console.log(saucisson[2])
+    const alcoholList =alcool.slice(0, 20);
+    const chipsNCo = apero.filter(c => c.nom_francais.includes('chips') || c.nom_francais.includes('Chips'))
+    const peanuts = apero.filter(c => c.nom_francais.includes('Cacahuète') && c.nom_francais.includes('grillée'));
+    const nutmix = apero.filter(c => c.nom_francais.includes('Mélange apéritif'));
+    // const saucisson = jsonData.filter(c => c.nom_francais.includes('Saucisson sec'));
+    // const olives = jsonData.filter(c => c.nom_francais.includes('Olive'));
     return (
         <div>
             toto
