f2572e88ff6447e716f9a3515520da3103f4dd4c
Claiirou <clairemachet@hotmail.fr>
2022-05-12 22:16:44 +0200

    add beer gif

--- src/pages/Calculateur.jsx
+++ src/pages/Calculateur.jsx
@@ -2,20 +2,34 @@ import React from "react";
 import brewery from "../assets/brewery.png";
 import aperitif from "../assets/aperitif.png";
 import "../styles/calculateur.css";
-import alcool from '../assets/alcools.json';
-import apero from '../assets/apero.json';
-import bieres from '../assets/bieres.json';
-import softs from '../assets/softs.json';
-import saucisson from '../assets/saucissonOlive.json';
-
+import ice from "../assets/ice.mp3";
+import beer from "../assets/beer.gif";
+// import alcool from "../assets/alcools.json";
+// import apero from "../assets/apero.json";
+// import bieres from "../assets/bieres.json";
+// import softs from "../assets/softs.json";
+// import saucisson from "../assets/saucissonOlive.json";
 
 function Calculateur() {
-  console.log(saucisson)
+  const sonIce = new Audio(ice);
+
+  const start = () => {
+    sonIce.play();
+  };
+
+  const [loading, setLoading] = React.useState(false);
+  const initTime = 5;
+  const [timer, setTimer] = React.useState(initTime);
+
+  const handleClick = () => {
+    start();
+    setLoading(true);
+    setTimer(initTime);
+  };
+
   return (
     <div className="calculateurContent">
-      <h1 className="titleCalculateur">
-        Alors, quoi de prévu pour l'apéro ?
-      </h1>
+      <h1 className="titleCalculateur">Alors, quoi de prévu pour l'apéro ?</h1>
       <div className="selectBottle">
         <img src={brewery} alt="brewery" className="breweryImg" />
         <div className="bottleContent">Est-ce qu'il y a de la place ?</div>
@@ -26,9 +40,10 @@ function Calculateur() {
           Est-ce qu'il y de la place ici aussi ?
         </div>
       </div>
-      <button type="submit" className="calculBtn">
+      <button type="submit" className="calculBtn" onClick={handleClick}>
         Calcul
       </button>
+      {loading && <img className="gifBeer" src={beer} alt="beergif"></img>}
     </div>
   );
 }
